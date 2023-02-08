9677ba86a8945d2ad625e52ffb7492e5079592f8
Noémie Goosen <noemie.goosen@gmail.com>
2023-02-08 16:43:09 +0100

    express - "num_train"
    router, model, controller

--- infrabel-express/routers/router.js
+++ infrabel-express/routers/router.js
@@ -7,6 +7,7 @@ const IncidentTypeRouter = require("./incident_typeRouter")
 const LieuRouter = require("./lieuRouter")
 const IncidentRouter = require("./incidentRouter")
 const RelationRouter = require("./relationRouter")
+const NumTrainRouter = require("./num_trainRouter")
 
 router.use("/api/v1/lignes", LigneRouter)
 router.use("/api/v1/arrets", ArretRouter)
@@ -15,6 +16,7 @@ router.use("/api/v1/incident_types", IncidentTypeRouter)
 router.use("/api/v1/lieux", LieuRouter)
 router.use("/api/v1/incidents", IncidentRouter)
 router.use("/api/v1/relations", RelationRouter)
+router.use("/api/v1/num_train", NumTrainRouter)
 
 router.all("*", (req, res) => {
     res.status(404).send("Erreur 404")
