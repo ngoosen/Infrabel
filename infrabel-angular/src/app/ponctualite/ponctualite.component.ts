ee60838c123fa360bbe1a49d54f5864d78fb37d5
baffi.adri <baffi.adri@gmail.com>
2023-02-20 01:22:22 +0100

    dimanche

--- infrabel-angular/src/app/ponctualite/ponctualite.component.ts
+++ infrabel-angular/src/app/ponctualite/ponctualite.component.ts
@@ -6,7 +6,8 @@ import { map,startWith } from 'rxjs';
 import { __values } from 'tslib';
 import { LigneArretService } from '../services/ligne-arret.service';
 import {InstantService} from'../services/instant.service';
-import {PonctualiteJ1Service} from '../services/ponctualite-j1.service';
+import {PonctualiteJ1Data, PonctualiteJ1Service} from '../services/ponctualite-j1.service';
+import {PonctualiteMomentService }from '../services/ponctualite-moment.service'
 
 
 
@@ -18,12 +19,24 @@ import {PonctualiteJ1Service} from '../services/ponctualite-j1.service';
 export class PonctualiteComponent implements OnInit {
 
 
-  constructor( private _ligneArretService: LigneArretService ,  private _InstantService :InstantService , private _Ponctulité: PonctualiteJ1Service){}
+  constructor(  private _ligneArretService: LigneArretService ,
+                private _InstantService :InstantService ,
+                private _PonctulitéMoins1: PonctualiteJ1Service,
+                private _Ponctualité:PonctualiteMomentService,
+
+
+                ){}
 
 
   options:string[]=[]
   instant: string[]=[]
-
+  ponctualite:number []=[]
+  statstop: any[] = []
+  ligne: any[]=[]
+  HeureD:any
+  HeureA:any
+  retardA:number=0
+  retardD:number=0
 
   mycontrol=new FormControl
 
@@ -35,6 +48,7 @@ export class PonctualiteComponent implements OnInit {
 
     //filtre recherche
   ngOnInit(): void {
+
     this._ligneArretService.getStops().subscribe( {
       next: (data) => {
         let temptab = data
@@ -53,6 +67,7 @@ export class PonctualiteComponent implements OnInit {
       }
     })
 
+      //moment de la journée
     this._InstantService.getInstants().subscribe({
       next: (inst)=>{
           let tempInst= inst
@@ -60,15 +75,81 @@ export class PonctualiteComponent implements OnInit {
           for(let opt of tempInst){
             this.instant.push(opt.fr_instant)
           }
+      },
+      error: (err) => {
+        console.log(err);
       }
     })
 
+      // le pourcentage de poncutalité
+    this._Ponctualité.getPonctuality().subscribe({
+      next:(ponct)=>{
+        let tempPonct= ponct
+
+        for (let pon of tempPonct){
+          this.ponctualite.push(pon.ponctualite_pourcentage)
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+
+      // ponctualite par stop j-1
+   this._PonctulitéMoins1.getPonctualityByStop(this.departControl.value).subscribe({
+      next: (data) => {
+
+   for(let item of data){
+        this.statstop.push(item)
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+
+
+}
+
+
+ // Renvoie tous les liens entre les lignes et les arrêts.
+ dataLigne(){
+ this._ligneArretService.getLineStops().subscribe({
+  next:(datas)=>{
+    for(let item of datas){
+      this.ligne.push(item)
+    }
+  },
+  error: (err) => {
+    console.log(err);
   }
+
+})
+}
+
+
+  // renvoie les Donnée J-1
+  datajmoins1(){
+  this._PonctulitéMoins1.getPonctuality().subscribe({
+    next:(data)=>{
+    let donnéponct=data
+
+    this.HeureA=donnéponct.arr_reel
+    this.HeureD=donnéponct.dep_reel
+    this.retardA=donnéponct.retard_arr
+    this.retardD=donnéponct.retard_dep
+      }
+
+  })
+}
 
     //fonction de filtrage des options
     private _filter(value:string){
       const filtervalue=value.toLowerCase()
       return this.options.filter(option=>option.toLocaleLowerCase().includes(filtervalue))
+
+
+
   }
 
   //
@@ -78,6 +159,13 @@ export class PonctualiteComponent implements OnInit {
     this.showResult = true;
   }
 
+
+  // fonction pour pas pouvoir cliquer sur bouton avant qui les input soit remplis
+  areInputsValid(): boolean {
+     return this.departControl.value && this.arriveeControl.value;
+      }
+
 }
+
 
 
